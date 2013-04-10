3049f86234f9d3d116c76be8d18ad616c145e3c2
Sean O'Hara <sohara@sohara.com>
2013-04-10 12:56:27 -0400

    Use rails AR validations to handle cdyne verification

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,12 +1,39 @@
 Spree::Address.class_eval do
   attr_accessor :cdyne_override
-  attr_accessible :cdyne_override, :cdyne_address_id
+  attr_accessor :is_shipping
+  attr_accessible :cdyne_override, :cdyne_address_id, :is_shipping
 
   has_one :cdyne_address, :class_name => "Spree::Address", :foreign_key => :cdyne_address_id
 
+  validate :must_by_cdyne_valid
+
+  def must_by_cdyne_valid
+    if is_shipping && country.iso3 == "USA"
+      cdyne_update
+      status_code=cdyne_address_response["ReturnCode"]
+      case status_code
+      when 1
+        errors.add(:base, I18n.t('cdyne.errors.1_invalid_input'))
+      when 2
+        raise "Invalid Cdyne License specified"
+      when 10
+        errors.add(:base, I18n.t('cdyne.errors.10_not_found'))
+        return true
+      when 100, 101, 200
+        return true
+      when 102, 202
+        errors.add(:address2, I18n.t('cdyne.errors.103_address_2_missing'))
+      when 103
+        errors.add(:address2, I18n.t('cdyne.errors.103_address_2_missing'))
+        return true
+      end
+    end
+  end
+
   def cdyne_update
-    return true unless self.country.iso3 == "USA"
+    # return true unless self.country.iso3 == "USA"
     corrected_address = self.cdyne_address_response
+    Rails.logger.info "Corrected address is #{corrected_address.inspect}"
 
     address = self.class.new
 
@@ -24,14 +51,16 @@ Spree::Address.class_eval do
     if address.save
       self.update_attribute(:cdyne_address_id, address.id)
     end
+    logger.info "CDYNE Address is #{address.inspect}"
   end
 
-  def cdyne_address_valid?
-    return true unless self.country.iso3 == "USA"
-    cdyne_address_status
-  end
+  # def cdyne_address_valid?
+  #   return true unless self.country.iso3 == "USA"
+  #   cdyne_address_status
+  # end
 
   def cdyne_address_status(status_code=cdyne_address_response["ReturnCode"])
+    Rails.logger.info "Response code is #{status_code}"
     case status_code
     when 2
       raise "Invalid Cdyne License specified"
@@ -39,23 +68,9 @@ Spree::Address.class_eval do
       return false
     when 100
       return true
-    end
-  end
-
-  def cdyne_address_description(status_code=cdyne_address_response["ReturnCode"])
-    case status_code
-    when 2
-      Rails.logger.error "Invalid Cdyne License specified"
-    when 10
-      "We are unable to find your address. Please correct the details supplied or select \"Use Original Address\" to use the current information."
-    when 100
-      "Address Confirmed"
-    when 101
-      "Address found but not verified"
-    when 102
-      "Primary Address Confirmed, cannot Validate Second Address. Please correct the details supplied or select \"Use Original Address\" to use the current information."
     when 103
-      "Primary Address Confirmed, \"Street Address (cont'd)\" field is missing."
+      errors.add(:address2, I18n.t('cdyne.errors.103_address_2_missing'))
+      return false
     end
   end
 
@@ -69,7 +84,7 @@ Spree::Address.class_eval do
   private
   def cdyne_query_hash
     {
-      :FirmOrRecipient => [firstname, lastname].join(" "), 
+      :FirmOrRecipient => [firstname, lastname].join(" "),
       :PrimaryAddressLine=> address1,
       :SecondaryAddressLine => address2,
       :CityName=> city,
