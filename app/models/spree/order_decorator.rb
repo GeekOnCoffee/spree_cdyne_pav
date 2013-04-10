c3d8293bc8795cbc7cc5f6ac950e57065d575ef8
Sean O'Hara <sohara@sohara.com>
2013-04-10 15:33:40 -0400

    Redefine clone billing address to set is_shipping = true on ship address

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,7 +1,18 @@
 module Spree
   class Order < ActiveRecord::Base
-    attr_accessible :use_corrected_address
-    attr_accessor :use_corrected_address
-    
+
+    # Redefine clone_billing_address to ensure that is_shipping
+    # is true on shipping attribute
+
+    def clone_billing_address
+      if bill_address and self.ship_address.nil?
+        self.ship_address = bill_address.clone
+      else
+        self.ship_address.attributes = bill_address.attributes.except('id', 'updated_at', 'created_at')
+      end
+      ship_address.is_shipping = true
+      true
+    end
+
   end
 end
