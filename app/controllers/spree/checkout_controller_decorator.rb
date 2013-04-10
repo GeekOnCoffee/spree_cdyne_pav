d7eb7b74e664492657de55f63d50924325f63002
Sean O'Hara <sohara@sohara.com>
2013-04-10 12:56:55 -0400

    Don't overried entire controller method, just alias and add required functionality

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,45 +1,15 @@
 Spree::CheckoutController.class_eval do
 
-  alias_method :update_without_cdyne, :update
+  alias_method :orig_update, :update
 
   def update
     if @order.state == "address"
-      if params[:order].delete(:use_original_address)  
-        Rails.logger.info("Checkout Update: use_original_address")
-        @order.update_attributes(object_params)
-        fire_event('spree.checkout.update')
-        @order.next
-      else
-        Rails.logger.info("Checkout Update: first_time")
-        @order.update_attributes(object_params)
-        fire_event('spree.checkout.update')
-        @order.shipping_address.cdyne_update
-
-        if @order.shipping_address.cdyne_address_valid? and @order.shipping_address.country.iso3 == "USA"
-          Rails.logger.info("Checkout Update: valid")
-          params[:order].delete("ship_address_attributes")
-          @order.ship_address.reload
-          cdyne_id = @order.shipping_address.cdyne_address_id
-
-          @order.next
-          @order.ship_address.reload
-          @order.reload
-          Rails.logger.info("Checkout Update: #{cdyne_id}")
-          @order.update_attribute(:ship_address_id, cdyne_id)
-          Rails.logger.info("Checkout Update: done with order.next")
-        elsif @order.shipping_address.country.iso3 == "USA"
-          Rails.logger.info("Checkout Update: error")
-          flash[:error] = @order.shipping_address.cdyne_address_description
-        else
-          @order.update_attributes(object_params)
-          fire_event('spree.checkout.update')
-          @order.next
-        end
-      end
-      respond_with(@order, :location => checkout_state_path(@order.state))
-      return
+      object_params[:ship_address_attributes][:is_shipping] = true
+      # @order.assign_attributes(object_params)
+      # @order.shipping_address.cdyne_update
+      #@order.shipping_address.cdyne_address_valid?
     end
-
-    update_without_cdyne
+    orig_update
   end
+
 end
